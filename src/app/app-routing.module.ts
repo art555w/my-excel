26af8124c70bb2c90d4924e0173425b5a142a2b4
Your Nameart555w <art555m@gmail.com>
2023-02-13 16:54:18 +0500

    finish html and css excel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TablePageComponent} from "./table-page/table-page.component";
+import {DashboardPageComponent} from "./dashboard-page/dashboard-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/table', pathMatch: 'full'},
+  {path: 'table', component: TablePageComponent},
+  {path: 'dashboard', component: DashboardPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
