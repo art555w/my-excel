fec4a1e53f017e5a64d1c82dde58089a4cf30ff9
Your Nameart555w <art555m@gmail.com>
2023-02-19 16:28:02 +0500

    finish select cell group by mousemove

--- src/app/shared/directives/select-cell.directive.ts
+++ src/app/shared/directives/select-cell.directive.ts
@@ -1,6 +1,9 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, HostListener, OnInit, Renderer2} from '@angular/core';
 import {SelectCellService} from "../services/select-cell.service";
-import {getByRange} from "../utils";
+import {getBorder, getByRange, getId} from "../utils";
+import {AllCellService} from "../services/all-cell.service";
+import {IBorder} from "../interface";
+import {TableComponent} from "../../components/table/table.component";
 
 @Directive({
   selector: '[appSelectCell]'
@@ -9,60 +12,112 @@ export class SelectCellDirective implements OnInit {
 
   currentId = ''
   lastId = ''
+  currentCell!: Element
+  grabCell!: Element
+  border!: IBorder
+  isGrabbing = false
+  ids: string[] = []
+  private unMousemove: any
+  private unSubMouseup: any
 
   constructor(
-    private el: ElementRef,
     private renderer: Renderer2,
-    private selectCellService: SelectCellService
-  ) {
-  }
-
+    private selectCellService: SelectCellService,
+    private allCellService: AllCellService,
+    private tableComponent: TableComponent
+  ) {}
   ngOnInit() {
-    const cell = this.selectCellService.selectCell('1:1')
-    this.renderer.addClass(cell, 'selected')
   }
 
+  @HostListener('mousedown', ['$event', '$event.target'])
+  onMousedown(event: MouseEvent, el: Element) {
+    if (el.getAttribute('data-type') === 'cell') {
+      if (!event.shiftKey) {
+        this.clear()
+        this.currentId = el.id
+        this.currentCell = this.selectCellService.selectCell(this.currentId)
+        this.grabCell = this.currentCell
 
-  @HostListener('mousedown', ['$event'])
-  onMousedown(event: MouseEvent) {
-    if (!event.shiftKey) {
-      this.clear()
-      this.currentId = this.el.nativeElement.id
-      const cell = this.selectCellService.selectCell(this.currentId)
-      this.renderer.addClass(cell, 'selected')
-    } else {
-      event.preventDefault()
-      this.clear()
-      this.lastId = this.el.nativeElement.id
-      this.currentId = this.selectCellService.current
+        this.selectCell(this.currentCell)
 
-      const cols = getByRange(
-        +this.lastId.split(':')[0],
-        +this.currentId.split(':')[0])
-      const rows = getByRange(
-        +this.lastId.split(':')[1],
-        +this.currentId.split(':')[1])
-
-
-      const ids = rows.reduce((acc:string[], row )=> {
-        cols.forEach((col) => {
-          acc.push(`${col}:${row}`)
+        this.unMousemove = this.renderer.listen('document', 'mousemove', (event) => {
+          this.lastId = event.target.dataset.type === 'cell' ? event.target.id : this.lastId
+          if (this.grabCell.id !== event.target.id) {
+            this.isGrabbing = true
+            this.clear()
+            this.grabCell = this.selectCellService.selectCell(this.lastId)
+            this.selectGroup(this.currentId, this.lastId)
+          }
         })
-        return acc
-      },[])
 
-      console.log(ids)
+      } else {
+        event.preventDefault()
+        this.clear()
+        this.isGrabbing = true
+        this.lastId = el.id
+        this.currentId = this.currentCell.id
 
-      this.selectCellService.selectGroup(ids).forEach((cell) => {
-        this.renderer.addClass(cell, 'selected')
+        this.selectGroup(this.currentId, this.lastId)
+
+      }
+      this.unSubMouseup = this.renderer.listen('document', 'mouseup', () => {
+        this.unMousemove()
+        if (this.isGrabbing) {
+          this.selectCellService.selectedGroup.forEach((cell) => {
+            if (this.border["border-top"].includes(cell.id)) {
+              this.renderer.addClass(cell, 'b-top')
+            }
+            if (this.border["border-right"].includes(cell.id)) {
+              this.renderer.addClass(cell, 'b-right')
+            }
+            if (this.border["border-left"].includes(cell.id)) {
+              this.renderer.addClass(cell, 'b-left')
+            }
+            if (this.border["border-bottom"].includes(cell.id)) {
+              this.renderer.addClass(cell, 'b-bottom')
+            }
+          })
+          this.renderer.addClass(this.currentCell, 'selected')
+          this.isGrabbing = false
+          this.unSubMouseup()
+        }
       })
     }
+  }
 
+  selectCell(currentEl: Element) {
+    this.renderer.addClass(currentEl, 'selected')
+  }
+  selectGroup(currentId: string, lastId: string) {
+    this.renderer.addClass(this.currentCell, 'selected')
+    const cols = getByRange(
+      getId(lastId).col,
+      getId(currentId).col)
+    const rows = getByRange(
+      getId(lastId).row,
+      getId(currentId).row)
+
+    this.ids = rows.reduce((acc: string[], row) => {
+      cols.forEach((col) => {
+        acc.push(`${col}:${row}`)
+      })
+      return acc
+    }, [])
+
+    this.border = getBorder(this.ids)
+    this.selectCellService.selectGroup(this.ids).forEach((cell) => {
+      this.renderer.addClass(cell, 'selected-group')
+    })
   }
 
   private clear() {
-    this.selectCellService.selectGroup(null).forEach(cell => {
+    this.allCellService.getAllCell().forEach(cell => {
+      this.renderer.removeClass(cell, 'selected-group')
       this.renderer.removeClass(cell, 'selected')
+      this.renderer.removeClass(cell, 'b-top')
+      this.renderer.removeClass(cell, 'b-right')
+      this.renderer.removeClass(cell, 'b-left')
+      this.renderer.removeClass(cell, 'b-bottom')
     })
     this.selectCellService.clear()
   }
